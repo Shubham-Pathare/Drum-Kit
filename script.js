536e05e82dac5ef656f7acf2dece0058a718c6ff
Shubham_Pathare <141949055+Shubham-Pathare@users.noreply.github.com>
2023-09-28 23:06:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+var Buttons = document.querySelectorAll(".button").length;
+
+for (var j = 0; j < Buttons; j++) {
+  document
+    .querySelectorAll(".button")
+    [j].addEventListener("click", function () {
+      var Styling = this.innerHTML;
+      sound(Styling);
+      animation(Styling);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+  sound(event.key);
+  animation(event.key);
+});
+
+function sound(key) {
+  switch (key) {
+    case "f":
+      var sound1 = new Audio("music/f.mp3");
+      sound1.play();
+      break;
+
+    case "a":
+      var sound2 = new Audio("music/a.mp3");
+      sound2.play();
+      break;
+
+    case "s":
+      var sound3 = new Audio("music/s.mp3");
+      sound3.play();
+      break;
+
+    case "d":
+      var sound4 = new Audio("music/d.mp3");
+      sound4.play();
+      break;
+
+    case "j":
+      var sound5 = new Audio("music/j.mp3");
+      sound5.play();
+      break;
+
+    case "k":
+      var sound6 = new Audio("music/k.mp3");
+      sound6.play();
+      break;
+
+    case "l":
+      var sound7 = new Audio("music/l.mp3");
+      sound7.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+function animation(currentKey) {
+  var mainButton = document.querySelector("." + currentKey);
+  mainButton.classList.add("animation");
+
+  setTimeout(function () {
+    mainButton.classList.remove("animation");
+  }, 100);
+}
